1b21523489d8bfe4f6fa8ed409901faecb4ea7ff
Khyta <dev@khyta.mozmail.com>
2023-01-11 20:15:27 +0100

    Refactors env fetching

--- script.py
+++ script.py
@@ -5,21 +5,28 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-try:
-  os.getenv('CLIENT_ID')
-  os.getenv('CLIENT_SECRET')
-  os.getenv('USER_AGENT')
-  os.getenv('REDDIT_USERNAME')
-  os.getenv('PASSWORD')
-except KeyError:
-  print('[error]: Missing environment variable(s)')
-  sys.exit(1)
+def fetch_env():
+  try:
+    os.getenv('CLIENT_ID')
+    client_id = os.getenv('CLIENT_ID')
+    os.getenv('CLIENT_SECRET')
+    client_secret = os.getenv('CLIENT_SECRET')
+    os.getenv('USER_AGENT')
+    user_agent = os.getenv('USER_AGENT')
+    os.getenv('REDDIT_USERNAME')
+    username = os.getenv('REDDIT_USERNAME')
+    os.getenv('PASSWORD')
+    password = os.getenv('PASSWORD')
+  except KeyError:
+    print('[error]: Missing environment variable(s)')
+    sys.exit(1)
+  return client_id, client_secret, user_agent, username, password
 
-client_id = os.getenv('CLIENT_ID')
-client_secret = os.getenv('CLIENT_SECRET')
-user_agent = os.getenv('USER_AGENT')
-username = os.getenv('REDDIT_USERNAME')
-password = os.getenv('PASSWORD')
+client_id = fetch_env()[0]
+client_secret = fetch_env()[1]
+user_agent = fetch_env()[2]
+username = fetch_env()[3]
+password = fetch_env()[4]
 
 reddit = praw.Reddit(
   client_id=client_id,
@@ -28,5 +35,3 @@ reddit = praw.Reddit(
   username=username,
   password=password
 )
-
-print(reddit.user.me())
