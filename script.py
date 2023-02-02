35478e820996fe541c85b770522eda51071246cc
Khyta <dev@khyta.mozmail.com>
2023-02-02 12:48:54 +0100

    CSV to dictionary

--- script.py
+++ script.py
@@ -401,6 +401,13 @@ def delete_posts(wiki_page_id, wiki_titles):
             df.drop(row_to_delete, inplace=True)
     df.to_csv(f'post_info_{wiki_page_id}.csv', index=False)
 
+def csv_to_dict(wiki_page_id):
+    # This function converts the CSV file to a dictionary
+    with open(f'post_info_{wiki_page_id}.csv', 'r') as file:
+        reader = csv.DictReader(file, fieldnames=['Title', 'ID', 'Current Post Hash', 'Current Flair Hash'])
+        headings = {row['Title']: row['ID'] for row in reader}
+    return headings
+
 def wiki_links_to_post(wiki_page_id):
 # This function will convert the links in the wiki page to the relevant post
 # links in the subreddit.
