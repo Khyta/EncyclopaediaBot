0999349d282fda48870bf14604bae9a0330e2fbf
Khyta <dev@khyta.mozmail.com>
2023-02-14 19:46:37 +0100

    Updates main.sh to pip3

--- main.sh
+++ main.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Check if the required libraries are already installed
-if pip freeze | grep -Fq -f requirements.txt; then
+if pip3 freeze | grep -Fq -f requirements.txt; then
   echo "Required libraries are already installed"
 else
   # Install the required libraries
-  pip install -r requirements.txt
+  pip3 install -r requirements.txt
   echo "Installed required libraries"
 fi
 
