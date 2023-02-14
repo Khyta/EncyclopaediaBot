b734198e0a84e013fbaab660b5d436f24db58072
Khyta <dev@khyta.mozmail.com>
2023-02-14 19:43:22 +0100

    Updates main.sh to be more efficient with pip libraries

--- main.sh
+++ main.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-# Install the required libraries
-pip install -r requirements.txt
-echo "Installed required libraries"
+# Check if the required libraries are already installed
+if pip freeze | grep -Fq -f requirements.txt; then
+  echo "Required libraries are already installed"
+else
+  # Install the required libraries
+  pip install -r requirements.txt
+  echo "Installed required libraries"
+fi
 
 # Run the main script
 python3 scripts/reddit_wiki_bot.py
